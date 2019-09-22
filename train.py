4caa231d5f4dd0d881c91724cf67c3221811e06b
MarcioFerreiraII <43908311+MarcioFerreiraII@users.noreply.github.com>
2019-09-22 13:10:08 -0300

    Mais algumas pequenas mundanças

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 #Aberttua do arquivo contendo a base de dados
 arq = open('novaBase.base', encoding="utf8") #Dados.base é minha base de dados
-listaDeDados = []
 tabela = pd.DataFrame()
 classe = []
 for linha in arq:
